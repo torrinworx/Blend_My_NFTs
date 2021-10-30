612658b3f99335b2f890c3abce9e7b6f7c73f052
Torrin Leonard <82110564+torrinworx@users.noreply.github.com>
2021-10-30 07:50:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ nftsPerBatch = 0    # Number of NFTs per batch
 renderBatch = 0     # The batch number to render in PNG-Generator
 imageName = "ThisCozyPlace_"    # The name of the NFT image produces by PNG-Generator
 
-# Save path of Blend_My_NFTs folder:
+# The path to Blend_My_NFTs folder:
 # Example mac: /Users/Path/to/Blend_My_NFTs
 # Example windows: C:\Users\Path\to\Blend_My_NFTs
 save_path_mac = ''
